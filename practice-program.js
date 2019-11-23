78f517865d8e329e6949a2629428216ec6fe8080
katthartic <katt.baum@gmail.com>
2019-11-23 08:57:47 -0500

    Renamed

---
+++ practice-program.js
@@ -0,0 +1,7 @@
+const reverseString = (str) => {
+    let reversedString = '';
+    for (let lastIndex = str.length - 1; lastIndex >= 0; lastIndex--) {
+      reversedString += str[lastIndex];
+    }
+      return reversedString
+  }
---
+++ practice-test-spec.js
@@ -0,0 +1,11 @@
+// Jasmine test specs
+
+describe('reverseString', function(){
+    it('is a function', function(){
+        expect(typeof reverseString).toBe('function')
+    })
+
+    it('returns a string', function(){
+        expect(typeof reverseString.toBe('string')
+    })
+})
