adaddd2bf627ccebbb33b98fffa2840fa78df78d
katthartic <katt.baum@gmail.com>
2019-11-30 23:42:39 +0000

    Update console tests

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const test = {
-	a:1,
-	b:2,
-  c:3
-}
-
 const traceTheChain = (obj) => {
   let arrOfProtos = []
   while (Object.getPrototypeOf(obj)) {
     obj = Object.getPrototypeOf(obj)
     arrOfProtos.push(obj)
-    console.log(arrOfProtos)
+    //console.log(arrOfProtos)
   }
   return arrOfProtos
 }
+
+console.log(traceTheChain([10, 5, 50]))
+console.log(traceTheChain("What objects are in my prototype chain?"))
